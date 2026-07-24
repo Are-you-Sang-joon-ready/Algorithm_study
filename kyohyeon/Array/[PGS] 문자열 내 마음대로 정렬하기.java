dec664688c3226d82a635299085cbb38785d15cf
hyeon <rygus0128@gmail.com>
2026-07-24 11:41:17 +0900

    [PGS] 문자열 내 마음대로 정렬하기 / 레벨1 / 11분 / 박교현

---
+++ kyohyeon/Array/[PGS] 문자열 내 마음대로 정렬하기.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+  public String[] solution(String[] strings, int n) {
+    String[] answer = strings.clone();
+
+    Arrays.sort(answer, (s1, s2) ->{
+      char c1 = s1.charAt(n);
+      char c2 = s2.charAt(n);
+
+      if(c1 == c2){
+        return s1.compareTo(s2);
+      }
+      return c1 - c2;
+    });
+    return answer;
+  }
+}
